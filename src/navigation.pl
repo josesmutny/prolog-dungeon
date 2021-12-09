73850f8deb8906706c57dd510cd1607031cd536c
José Smutný <smutnjo2@fit.cvut.cz>
2021-12-09 19:18:07 +0100

    Add print operations to navigation

--- src/navigation.pl
+++ src/navigation.pl
@@ -2,43 +2,49 @@
     north/0,
     south/0,
     east/0,
-    west/0
+    west/0,
+    north_door/2,
+    south_door/2,
+    east_door/2,
+    west_door/2
 ]).
 :- use_module(common).
+:- use_module(custom_writes).
+:- use_module(security).
+:- use_module(doors).
+
 % Move functions ==================================================================================
 
 move(X, Y, X1, Y1):-
     write_position(X1, Y1), nl,
     retract(position(X, Y)),
-    assert(position(X1, Y1)).
+    assert(position(X1, Y1)),
+    write_status().
 
 can_move(Xnext, Ynext):-
+    (
+        not(is_dead()), !;
+        is_dead(), write("Dead people cannot move :("), false, !
+    ),
     position(X, Y),
     has_keys(Keys),
     member(Key, Keys),
     (door(X, Y, Xnext, Ynext, Lock); door(Xnext, Ynext, X, Y, Lock)),
     opens(Key, Lock).
 
-% North ===============================================================================================
-can_move_north():-
-	position(X, Y),
-	Ynext is Y + 1,
-    can_move(X, Ynext).
-
-move_north(X, Y):-
-	Ynext is Y + 1,
-	write("Moving north: "),
-    move(X, Y, X, Ynext).
-
 is_north(X, Y, X1, Y1):- X1 is X    , Y1 is Y + 1.
 is_south(X, Y, X1, Y1):- X1 is X    , Y1 is Y - 1.
 is_east(X, Y, X1, Y1):-  X1 is X - 1, Y1 is Y.
 is_west(X, Y, X1, Y1):-  X1 is X + 1, Y1 is Y.
 
+north_door(X, Y):- is_north(X, Y, X1, Y1), (door(X, Y, X1, Y1, _), !; door(X1, Y1, X, Y, _), !).
+south_door(X, Y):- is_south(X, Y, X1, Y1), (door(X, Y, X1, Y1, _), !; door(X1, Y1, X, Y, _), !).
+east_door(X, Y):- is_east(X, Y, X1, Y1), (door(X, Y, X1, Y1, _), !; door(X1, Y1, X, Y, _), !).
+west_door(X, Y):- is_west(X, Y, X1, Y1), (door(X, Y, X1, Y1, _), !; door(X1, Y1, X, Y, _), !).
 
 move_player(X, Y, X1, Y1):-
     can_move(X1, Y1), move(X, Y, X1, Y1), !;
-    not(door(X, Y, X1, Y1, _)), write("There is no door."), nl, !;
+    not(door(X, Y, X1, Y1, _)), custom_writes:is_long_delay(Delay), write_delayed("There is no door.\n", Delay), !;
     door(X, Y, X1, Y1, Lock), opens(Key, Lock), has_keys(Keys), not(member(Key, Keys)), write("You do not have the right key."), nl, !.
 
 north():- position(X, Y), is_north(X, Y, X1, Y1), move_player(X, Y, X1, Y1).
