871cb68a97bd6aea0c26f87faa82629593ae0324
José Smutný <smutnjo2@fit.cvut.cz>
2021-12-09 19:15:49 +0100

    Add open property to door

--- src/security.pl
+++ src/security.pl
@@ -1,7 +1,9 @@
 :- module(security, [
     lock_is_gold/1,
     lock_is_iron/1,
-    is_locked/1,
+    lock_is_open/1,
+    door_is_locked/1,
+    door_is_open/1,
     key_is_gold/1,
     key_is_iron/1,
     opens/2
@@ -10,10 +12,13 @@
 
 lock_is_iron(1).
 lock_is_gold(2).
+lock_is_open(0).
 
-is_locked(door(_, _, _, _, Lock)):- not( Lock is  0).
+door_is_locked(door(_, _, _, _, Lock)):- not(lock_is_open(Lock)).
+door_is_open(door(_, _, _, _, Lock)):- lock_is_open(Lock).
 
 key_is_iron(1).
 key_is_gold(2).
 
+opens(_, Lock):- lock_is_open(Lock).
 opens(Key, Lock):- key_is_iron(Key), lock_is_iron(Lock), !; key_is_gold(Key), lock_is_gold(Lock).
