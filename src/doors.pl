63ac9c52bd54784292d5e74602372b92da17c774
José Smutný <smutnjo2@fit.cvut.cz>
2021-12-12 12:31:43 +0100

    Reorganize initial layout

--- src/doors.pl
+++ src/doors.pl
@@ -1,7 +1,7 @@
 :- module(doors, [door/5, room/5]).
 :- use_module(security).
 :- use_module(attack).
-
+:- use_module(teleport).
 /*
             X - X i X
             |   |   |
@@ -18,6 +18,8 @@
 
 */
 
+:- dynamic door/5.
+
 door(0, 0, 0, 1, Lock):- lock_is_open(Lock).
 door(0, 1, 1, 1, Lock):- lock_is_iron(Lock).
 door(0, 1, -1, 1, Lock):- lock_is_open(Lock).
@@ -28,16 +30,19 @@ door(0, -1, 1, -1, Lock):- lock_is_gold(Lock).
 door(1, -1, 1, 0, Lock):- lock_is_open(Lock).
 door(1, 0, 1, 1, Lock):- lock_is_open(Lock).
 
-% room(X, Y, Items, Description)
+:- dynamic room/5.
+% room(X, Y, Items, Enemies, Description)
+room(-1, 1, [Teleport], Empty, "North west"):- no_enemy(Empty), is_teleport(Teleport, 0, 0).
+room(0, 1, [Key, Weapon], Empty, "North"):- key_is_gold(Key), no_enemy(Empty), is_axe(Weapon).
+room(1, 1, [], Empty, "North east"):- no_enemy(Empty).
 
-room(0, 0, [], Empty, "You find yourself in a cold, empty, humid room. The only light comes from a torch in your hand."):- no_enemy(Empty).
-room(-1, 1, [], Empty, "Upper left"):- no_enemy(Empty).
-room(-1, 0, [], Empty, "Center left"):- no_enemy(Empty).
-room(-1, -1, [], Empty, "Bottom left"):- no_enemy(Empty).
-room(0, -1, [Key], Empty, "Bottom center"):- key_is_iron(Key), no_enemy(Empty).
-room(1, -1, [], Empty, "Bottom right"):- no_enemy(Empty).
-room(0, 1, [Key], Empty, "Center right"):- key_is_gold(Key), no_enemy(Empty).
-room(1, 1, [], Empty, "Top right"):- no_enemy(Empty).
+room(-1, 0, [], Empty, "West"):- no_enemy(Empty).
+room(0, 0, [], Empty, "Center"):- no_enemy(Empty).
+room(1, 0, [], Empty, "East"):- no_enemy(Empty).
+
+room(-1, -1, [], Empty, "South west"):- no_enemy(Empty).
+room(0, -1, [Key], Empty, "South"):- key_is_iron(Key), no_enemy(Empty).
+room(1, -1, [], Empty, "South east"):- no_enemy(Empty).
 
 % for some reason, this makes prolog loop forever checking alternation door directions
 % door(X, Y, X1, Y1, Lock):- door(X1, Y1, X, Y, Lock), !; door(X, Y, X1, Y1, Lock).
