506205097727eb3039fce0922a1149b0c87d6ca5
José Smutný <smutnjo2@fit.cvut.cz>
2021-12-18 16:50:34 +0100

    Write room when starting game

--- main.pl
+++ main.pl
@@ -13,4 +13,4 @@
     write_delayed("You are trapped in a dungeon, from whence only few have come alive.\n"),
     write_delayed("I shall hold you no further. Use north()., south(). east(). and west(). to move, and collect the right keys to open the right dooors.\n"),
     write_delayed("Good luck!\n", 0.2, 2), nl, */
-    write_status().
+    write_status(), nl, write_room().
